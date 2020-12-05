102053e1e5aee4dc7472b5a14991bf9b3570fa70
Jason Schweier <jason.schweier@gmail.com>
2020-12-04 20:36:29 -0500

    Add more helpers when using iex

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,13 @@
 alias Kconnectex.{Cluster, Connectors}
 
+defmodule Helpers do
+  def filestream_config(topic \\ "license-stream", file \\ "/kafka/LICENSE") do
+    %{
+      "connector.class" => "FileStreamSource",
+      "topic" => topic,
+      "file" => file
+    }
+  end
+end
+
 client = Kconnectex.client("http://0.0.0.0:8083")
