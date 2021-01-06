f7710d278789059708576ce800892c8684ab52fb
Jason Schweier <jason.schweier@gmail.com>
2021-01-06 18:53:34 -0500

    Clean up Cluster tests

--- test/kconnectex/cluster_test.exs
+++ test/kconnectex/cluster_test.exs
@@ -19,25 +19,19 @@ defmodule Kconnectex.ClusterTest do
     end
   end
 
-  test "GET /" do
-    assert Kconnectex.Cluster.info(client()) == %{
-             "version" => "5.5.0",
-             "commit" => "e5741b90cde98052",
-             "kafka_cluster_id" => "I4ZmrWqfT2e-upky_4fdPA"
-           }
+  test "GET / with no connection" do
+    assert Kconnectex.Cluster.info(client("badconn")) == {:error, :econnrefused}
   end
 
   @tag :integration
-  test "GET / integration" do
+  test "GET /" do
     import IntegrationHelpers
 
     cluster_info = Kconnectex.Cluster.info(connect_client())
 
     assert cluster_info["version"] == "2.6.0"
-  end
-
-  test "GET / with no connection" do
-    assert Kconnectex.Cluster.info(client("badconn")) == {:error, :econnrefused}
+    assert Map.has_key?(cluster_info, "commit")
+    assert Map.has_key?(cluster_info, "kafka_cluster_id")
   end
 
   defp client(base_url \\ "localhost") do
