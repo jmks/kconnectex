a46f29ea85f2b8ce8b285c36f07fed7ffecc3f54
Jason Schweier <jason.schweier@gmail.com>
2021-01-29 09:55:42 -0500

    Update Admin tests

--- test/kconnectex/admin_test.exs
+++ test/kconnectex/admin_test.exs
@@ -3,37 +3,31 @@ defmodule Kconnectex.AdminTest do
 
   defmodule FakeAdapter do
     def call(%{method: :put, url: "localhost/admin/loggers/root"}, _) do
-      {:ok,
-       %Tesla.Env{
-         status: 200,
-         body: ["root"]
-       }}
+      {:ok, %Tesla.Env{status: 200, body: ["root"]}}
     end
   end
 
   test "PUT /admin/loggers/:logger" do
-    assert Kconnectex.Admin.logger_level(client(), "root", "DEBUG") == [
-             "root"
-           ]
+    assert {:ok, ["root"]} == Kconnectex.Admin.logger_level(client(), "root", "DEBUG")
   end
 
   @tag :integration
   test "loggers" do
     import IntegrationHelpers
 
-    # Reset to defaults; previous test runs may have changed them
+    # Reset to known values; previous test runs may have changed them
     Kconnectex.Admin.logger_level(connect_client(), "root", "INFO")
     Kconnectex.Admin.logger_level(connect_client(), "org.reflections", "WARN")
 
-    loggers = Kconnectex.Admin.loggers(connect_client())
+    {:ok, loggers} = Kconnectex.Admin.loggers(connect_client())
     assert %{"org.reflections" => %{"level" => "WARN"}, "root" => %{"level" => "INFO"}} = loggers
 
-    level = Kconnectex.Admin.logger_level(connect_client(), "root")
+    {:ok, level} = Kconnectex.Admin.logger_level(connect_client(), "root")
     assert %{"level" => "INFO"} = level
 
     Kconnectex.Admin.logger_level(connect_client(), "root", "DEBUG")
 
-    level = Kconnectex.Admin.logger_level(connect_client(), "root")
+    {:ok, level} = Kconnectex.Admin.logger_level(connect_client(), "root")
     assert %{"level" => "DEBUG"} = level
   end
 
