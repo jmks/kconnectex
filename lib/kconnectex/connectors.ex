d1b8507d647ee7e7343d3799c140d6ca4ead2571
Jason Schweier <jason.schweier@gmail.com>
2021-02-11 17:47:27 -0500

    Rename fun

--- lib/kconnectex/connectors.ex
+++ lib/kconnectex/connectors.ex
@@ -9,60 +9,60 @@ defmodule Kconnectex.Connectors do
   end
 
   def create(client, connector, config) do
-    request(client, connector)
+    request_with_connector(client, connector)
     |> Request.post("/connectors", %{name: connector, config: config})
     |> Request.execute()
   end
 
   def info(client, connector) do
-    request(client, connector)
+    request_with_connector(client, connector)
     |> Request.get("/connectors/#{connector}")
     |> Request.execute()
   end
 
   def config(client, connector) do
-    request(client, connector)
+    request_with_connector(client, connector)
     |> Request.get("/connectors/#{connector}/config")
     |> Request.execute()
   end
 
   def update(client, connector, config) do
-    request(client, connector)
+    request_with_connector(client, connector)
     |> Request.put("/connectors/#{connector}/config", config)
     |> Request.execute()
   end
 
   def status(client, connector) do
-    request(client, connector)
+    request_with_connector(client, connector)
     |> Request.get("/connectors/#{connector}/status")
     |> Request.execute()
   end
 
   def restart(client, connector) do
-    request(client, connector)
+    request_with_connector(client, connector)
     |> Request.post("/connectors/#{connector}/restart", "")
     |> Request.execute()
   end
 
   def pause(client, connector) do
-    request(client, connector)
+    request_with_connector(client, connector)
     |> Request.put("/connectors/#{connector}/pause", "")
     |> Request.execute()
   end
 
   def resume(client, connector) do
-    request(client, connector)
+    request_with_connector(client, connector)
     |> Request.put("/connectors/#{connector}/resume", "")
     |> Request.execute()
   end
 
   def delete(client, connector) do
-    request(client, connector)
+    request_with_connector(client, connector)
     |> Request.delete("/connectors/#{connector}")
     |> Request.execute()
   end
 
-  defp request(client, connector) do
+  defp request_with_connector(client, connector) do
     client
     |> Request.new()
     |> Request.validate({:present, connector}, "connector can not be blank")
