4bebc78152830e86fb6376bd9b703d2a6a52ae21
Jason Schweier <jason.schweier@gmail.com>
2021-02-10 19:50:32 -0500

    Delete dead validation code

--- lib/kconnectex/util.ex
+++ lib/kconnectex/util.ex
@@ -17,39 +17,4 @@ defmodule Kconnectex.Util do
         {:error, err}
     end
   end
-
-  def validate(validations) do
-    do_validate(validations, [])
-  end
-
-  defp do_validate([], []), do: :ok
-
-  defp do_validate([], errors), do: {:error, errors |> List.flatten() |> Enum.reverse()}
-
-  defp do_validate([{field, checks} | rest], errors) do
-    case failed_conditions(field, checks) do
-      [] ->
-        do_validate(rest, errors)
-
-      errs ->
-        do_validate(rest, [errs | errors])
-    end
-  end
-
-  defp failed_conditions(field, checks) do
-    checks
-    |> Enum.map(fn condition ->
-      case check(condition) do
-        :ok -> :ok
-        {:error, err} -> {field, err}
-      end
-    end)
-    |> Enum.filter(&(&1 != :ok))
-  end
-
-  defp check({:present, ""}), do: {:error, :is_blank}
-  defp check({:present, __}), do: :ok
-
-  defp check({:match, value, value}), do: :ok
-  defp check({:match, _, _}), do: {:error, :do_not_match}
 end
