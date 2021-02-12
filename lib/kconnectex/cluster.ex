549dad91865fcbe230c2e9b58fcef27a85c06b1c
Jason Schweier <jason.schweier@gmail.com>
2021-02-12 18:21:12 -0500

    Cluster docs

--- lib/kconnectex/cluster.ex
+++ lib/kconnectex/cluster.ex
@@ -7,6 +7,24 @@ defmodule Kconnectex.Cluster do
 
   alias Kconnectex.Request
 
+  @doc """
+  Displays basic information for the Kafka Connect cluster.
+
+  ## Parameters
+
+    - client: client from `Kconnectex.client/1`
+
+  ## Examples
+
+      > Kconnectex.Cluster.info(client)
+
+      {:ok,
+       %{
+         "commit" => "62abe01bee039651",
+         "kafka_cluster_id" => "DPyXlcLLQqSfvYLLxCKUoQ",
+         "version" => "2.6.0"
+       }}
+  """
   def info(client) do
     Request.new(client)
     |> Request.get("/")
