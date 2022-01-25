cd72d784b2323977134781af33ae0fb72afe1c3b
Jason Schweier <jason.schweier@gmail.com>
2022-01-25 10:23:22 -0500

    cli: Test when writing config file fails

--- test/kconnectex/cli/config_file_test.exs
+++ test/kconnectex/cli/config_file_test.exs
@@ -25,9 +25,10 @@ defmodule Kconnectex.CLI.ConfigFileTest do
   end
 
   describe ".validate_config" do
-    test "errors without a host" do
-      assert {:error, reason} =
-               ConfigFile.validate_config(%{"clusters" => %{"local" => %{"port" => 8083}}})
+    setup :config_without_host
+
+    test "errors without a host", %{invalid_config: config} do
+      assert {:error, reason} = ConfigFile.validate_config(config)
 
       assert ConfigFile.format_error(reason) == "cluster local must specify a host"
     end
@@ -49,14 +50,17 @@ defmodule Kconnectex.CLI.ConfigFileTest do
     end
 
     setup :valid_config
+
     test "ok with valid configuration", %{valid_config: config} do
       assert {:ok, _} = ConfigFile.validate_config(config)
     end
   end
 
-  @tag :tmp_dir
   describe ".write" do
+    @describetag :tmp_dir
+
     setup :valid_config
+
     test "writes a valid configuration to disk", %{tmp_dir: dir, valid_config: config} do
       filepath = Path.join([dir, "serialized.json"])
 
@@ -68,6 +72,15 @@ defmodule Kconnectex.CLI.ConfigFileTest do
       assert read_config[:config_file_path] == filepath
       assert Map.delete(read_config, :config_file_path) == config
     end
+
+    setup :config_without_host
+
+    test "errors when configuration is invalid", %{tmp_dir: dir, invalid_config: config} do
+      filepath = Path.join([dir, "serialized.json"])
+
+      assert {:error, reason} = ConfigFile.write(config, filepath)
+      assert reason == {:missing_host, "local"}
+    end
   end
 
   defp fixture(filename) do
@@ -78,5 +91,9 @@ defmodule Kconnectex.CLI.ConfigFileTest do
     Map.put(context, :valid_config, %{
       "clusters" => %{"local" => %{"host" => "localhost", "port" => 8083}}
     })
+  end
+
+  def config_without_host(context) do
+    Map.put(context, :invalid_config, %{"clusters" => %{"local" => %{"port" => 8083}}})
   end
 end
