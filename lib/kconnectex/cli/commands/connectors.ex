cc23ee7bdbfa67187666ca51193b21a7611f9049
Jason Schweier <jason.schweier@gmail.com>
2025-10-21 21:29:45 -0400

    Sort tasks by id

--- lib/kconnectex/cli/commands/connectors.ex
+++ lib/kconnectex/cli/commands/connectors.ex
@@ -10,18 +10,26 @@ defmodule Kconnectex.CLI.Commands.Connectors do
       status["connector"]["state"]
     ]
 
-    # TODO: rows should be sorted by id
-    task_rows = Enum.flat_map(status["tasks"], fn task ->
-      task_row = [status["name"], "TASK", to_string(task["id"]), task["worker_id"], task["state"]]
+    task_rows =
+      status["tasks"]
+      |> Enum.sort_by(fn task -> task["id"] end)
+      |> Enum.flat_map(fn task ->
+        task_row = [
+          status["name"],
+          "TASK",
+          to_string(task["id"]),
+          task["worker_id"],
+          task["state"]
+        ]
 
-      if Map.has_key?(task, "trace") do
-        trace = String.split(task["trace"], "\n")
+        if Map.has_key?(task, "trace") do
+          trace = String.split(task["trace"], "\n")
 
-        [task_row | [{:lines, trace}]]
-      else
-        [task_row]
-      end
-    end)
+          [task_row | [{:lines, trace}]]
+        else
+          [task_row]
+        end
+      end)
 
     [connector | task_rows]
   end
